fa8ffe3448dd33978840cf040647daa0ca8c4f9b
BrunoEnriqueB <brunobaronenrique@gmail.com>
2021-10-05 21:38:00 -0300

    Aula 21 - Strings avançadas

---
+++ JavaScript-Mateus_Battisti/projetos/Sessao_2/Aulas/21_string_avancado/scripts.js
@@ -0,0 +1,5 @@
+console.log("Primeira Linha\nSegunda Linha");
+console.log('Imprimir "aspas duplas" na string');
+console.log("Imprimir 'aspas simples na string");
+console.log(`A multiplicação de 5 por 3 é ${5 * 3}`);
+console.log("O " + "meu " + "nome " + "é " + "Mateus ");
