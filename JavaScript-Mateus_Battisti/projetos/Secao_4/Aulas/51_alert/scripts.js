480140491fc55974612848483692427016ae87fe
BrunoEnriqueB <brunobaronenrique@gmail.com>
2021-10-07 21:41:42 -0300

    Seção 4 - Exercícios

---
+++ JavaScript-Mateus_Battisti/projetos/Secao_4/Aulas/51_alert/scripts.js
@@ -0,0 +1,1 @@
+alert("Isso aqui não é legal");
---
+++ JavaScript-Mateus_Battisti/projetos/Secao_4/Aulas/52_math/scripts.js
@@ -0,0 +1,12 @@
+let maior = Math.max(1,6,3,2,7);
+
+console.log(maior);
+
+let x = 10;
+
+if (true) {
+    console.log(x);
+    x = 20;
+}
+
+console.log(x)
